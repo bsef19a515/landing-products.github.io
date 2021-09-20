3862e1e1ce634c79e493b1b98a8047b6e2441214
kinza <bsef19a515@pucit.edu.pk>
2021-09-20 17:51:08 +0500

    studio-web

---
+++ app.js
@@ -0,0 +1,8 @@
+var showthem= document.getElementsById("showt");
+function show(){
+   showt.style.display="block";
+   showt.style.left="3%";
+}
+function hide(){
+    showt.style.left= "-150%";
+}
